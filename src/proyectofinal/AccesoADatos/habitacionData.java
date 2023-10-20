ca05a9bc47b8adc6870ef71398c6e49e9d10a92c
Neopl4st <chrisjava2@gmail.com>
2023-10-20 13:10:21 -0300

    20-10
    habitacionData
    agregar habitaciones desde el Main

--- src/proyectofinal/AccesoADatos/habitacionData.java
+++ src/proyectofinal/AccesoADatos/habitacionData.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -11,41 +12,44 @@ import proyectofinal.Entidades.Categoria;
 import proyectofinal.Entidades.habitaciones;
 
 public class habitacionData {
-    private Connection con= null;
+
+    private Connection con = null;
 
     public habitacionData() {
-        con= Conexion.getConexion();
+        con = Conexion.getConexion();
     }
-    
-    public void agregarHabitacion(habitaciones habitaciones){
-        String sql= "INSERT INTO habitaciones (Tipo, Disponibilidad) VALUES (?, ?)";
+
+    public void agregarHabitacion(habitaciones habitaciones) {
+        String sql = "INSERT INTO habitaciones (Tipo, Disponibilidad) VALUES (?, ?)";
         try {
-            PreparedStatement ps= con.prepareStatement(sql);
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setInt(1, habitaciones.getTipo());
             ps.setBoolean(2, false);
-            ps.executeUpdate(sql);
-            ResultSet rs= ps.getGeneratedKeys();
-            if(rs.next()){
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
                 habitaciones.setIdHabitacion(rs.getInt(1));
-                JOptionPane.showMessageDialog(null, "Habitación Guardada");
+//                JOptionPane.showMessageDialog(null, "Habitación Guardada");
             }
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla");
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de habitaciones.");
         }
     }
-    
-    public void cambiarPrecio(Categoria categoria){
-        String sql= "UPDATE categoria SET precio WHERE codigo= ?";
+
+    public void cambiarPrecio(Categoria categoria) {
+        String sql = "UPDATE categoria SET precio WHERE codigo= ?";
         try {
-            PreparedStatement ps= con.prepareStatement(sql);
+            PreparedStatement ps = con.prepareStatement(sql);
             ps.setDouble(1, categoria.getPrecio());
-            int exito= ps.executeUpdate();
-            if(exito== 1){
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
                 JOptionPane.showMessageDialog(null, "Precio Modificado");
             }
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla");
         }
-        
     }
+
+//  
+    
 }
--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -3,55 +3,116 @@ package proyectofinal;
 import java.sql.Connection;
 import proyectofinal.AccesoADatos.CategoriaData;
 import proyectofinal.AccesoADatos.Conexion;
+import proyectofinal.AccesoADatos.habitacionData;
 import proyectofinal.AccesoADatos.huespedData;
 import proyectofinal.Entidades.Categoria;
+import proyectofinal.Entidades.habitaciones;
 import proyectofinal.Entidades.huesped;
 
 public class ProyectoFinal {
 
     public static void main(String[] args) {
         Conexion.getConexion();
-        huespedData x= new huespedData();
-        huesped ej= new huesped(123, "Rodriguez", "Santiago", "Calle 2", "ej@hotmail.com", 1234567, true);
-        huesped santiago= new huesped(321, "Gonzalez", "Horacio", "Calle 3", "horaciog@gmail.com", 7654321, true);
-        huesped prueba= new huesped(101, "Ejemplo", "Damian", "Calle 4", "p@hotmail.com", 2345678, true);
-        //x.agregarHuesped(ej);
-        //x.agregarHuesped(santiago);
-        //x.agregarHuesped(prueba);
-        huesped ejemplo= new huesped(1234567, "Gonzalez", "Uriel", "Calle 7", "ejemplo@gmail.com", 1176588932, true);
-        //x.agregarHuesped(ejemplo);
+//        huespedData x = new huespedData();
+//        huesped ej = new huesped(123, "Rodriguez", "Santiago", "Calle 2", "ej@hotmail.com", 1234567, true);
+//        huesped santiago = new huesped(321, "Gonzalez", "Horacio", "Calle 3", "horaciog@gmail.com", 7654321, true);
+//        huesped prueba = new huesped(101, "Ejemplo", "Damian", "Calle 4", "p@hotmail.com", 2345678, true);
+//        //x.agregarHuesped(ej);
+//        //x.agregarHuesped(santiago);
+//        //x.agregarHuesped(prueba);
+//        huesped ejemplo = new huesped(1234567, "Gonzalez", "Uriel", "Calle 7", "ejemplo@gmail.com", 1176588932, true);
+//        //x.agregarHuesped(ejemplo);
+//
+//        CategoriaData y = new CategoriaData();
+//
+//        Categoria A = new Categoria(211, 2, 1, 1, 0);
+//        Categoria B = new Categoria(221, 2, 2, 1, 0);
+//        Categoria C = new Categoria(212, 2, 1, 2, 0);
+//        //---------------Habitaciones Dobles--------------------------
+//        Categoria D = new Categoria(421, 4, 2, 1, 0);
+//        Categoria E = new Categoria(422, 4, 2, 2, 0);
+//        Categoria F = new Categoria(441, 4, 4, 1, 0);
+//        //---------------Habitaciones Triples--------------------------
+//        Categoria G = new Categoria(631, 6, 3, 1, 0);
+//        Categoria H = new Categoria(632, 6, 3, 2, 0);
+//        Categoria I = new Categoria(661, 6, 6, 1, 0);
+//        //---------------Suite Lujo--------------------------
+//        Categoria J = new Categoria(213, 2, 1, 3, 0);
+//        Categoria K = new Categoria(223, 2, 2, 3, 0);
+//
+//        y.agregarCategoria(A);
+//        y.agregarCategoria(B);
+//        y.agregarCategoria(C);
+//        y.agregarCategoria(D);
+//        y.agregarCategoria(E);
+//        y.agregarCategoria(F);
+//        y.agregarCategoria(G);
+//        y.agregarCategoria(H);
+//        y.agregarCategoria(I);
+//        y.agregarCategoria(J);
+//        y.agregarCategoria(K);
+
+        habitacionData data = new habitacionData();
+//        habitaciones habitacionSimple1 = new habitaciones(211, true);
+        habitaciones habitacionSimple2 = new habitaciones(221, true);
+        habitaciones habitacionSimple3 = new habitaciones(212, true);
+        int i;
+//        for (i = 1; i <= 35; i++) {
+//            data.agregarHabitacion(habitacionSimple1);
+//        }
+
         
-        CategoriaData y= new CategoriaData();
+        for (i = 36; i <= 70; i++) {
+            data.agregarHabitacion(habitacionSimple2);
+        }
+
+        for (i = 71; i <= 100; i++) {
+            data.agregarHabitacion(habitacionSimple3);
+        }
+
+        habitaciones habitacionDoble1 = new habitaciones(421, true);
+        habitaciones habitacionDoble2 = new habitaciones(422, true);
+        habitaciones habitacionDoble3 = new habitaciones(441, true);
+        for (i = 101; i <= 120; i++) {
+            
+            data.agregarHabitacion(habitacionDoble1);
+        }
+        for (i = 121; i <= 140; i++) {
+            
+            data.agregarHabitacion(habitacionDoble2);
+        }
+        for (i = 141; i <= 150; i++) {
+            
+            data.agregarHabitacion(habitacionDoble3);
+        }
+
+       habitaciones habitacionTriple1 = new habitaciones(631, true);
+        habitaciones habitacionTriple2 = new habitaciones(632, true);
+        habitaciones habitacionTriple3 = new habitaciones(661, true);
+        for (i = 151; i <= 165; i++) {
+            
+            data.agregarHabitacion(habitacionTriple1);
+        }
+        for (i = 166; i <= 180; i++) {
+            
+            data.agregarHabitacion(habitacionTriple2);
+        }
+        for (i = 181; i <= 190; i++) {
+            
+            data.agregarHabitacion(habitacionTriple3);
+        }
         
-    Categoria A = new Categoria(211, 2, 1, 1, 0);
-    Categoria B = new Categoria(221, 2, 2, 1, 0);
-    Categoria C = new Categoria(212, 2, 1, 2, 0);
-    //---------------Habitaciones Dobles--------------------------
-    Categoria D = new Categoria(421, 4, 2, 1, 0);
-    Categoria E = new Categoria(422, 4, 2, 2, 0);
-    Categoria F = new Categoria(441, 4, 4, 1, 0);
-    //---------------Habitaciones Triples--------------------------
-    Categoria G = new Categoria(631, 6, 3, 1, 0);
-    Categoria H = new Categoria(632, 6, 3, 2, 0);
-    Categoria I = new Categoria(661, 6, 6, 1, 0);
-    //---------------Suite Lujo--------------------------
-    Categoria J = new Categoria(213, 2, 1, 3, 0);
-    Categoria K = new Categoria(223, 2, 2, 3, 0);
-    
-    /*y.agregarCategoria(A);
-    y.agregarCategoria(B);
-    y.agregarCategoria(C);
-    y.agregarCategoria(D);
-    y.agregarCategoria(E);
-    y.agregarCategoria(F);
-    y.agregarCategoria(G);
-    y.agregarCategoria(H);
-    y.agregarCategoria(I);
-    y.agregarCategoria(J);
-    y.agregarCategoria(K);*/
-    
-        
-        
+         habitaciones habitacionSuite = new habitaciones(213, true);
+        habitaciones habitacionSuite2 = new habitaciones(223, true);
+
+        for (i = 191; i <= 195; i++) {
+            
+            data.agregarHabitacion(habitacionSuite);
+        }
+        for (i = 196; i <= 200; i++) {
+            
+            data.agregarHabitacion(habitacionSuite2);
+        }
     }
-    
+
 }
